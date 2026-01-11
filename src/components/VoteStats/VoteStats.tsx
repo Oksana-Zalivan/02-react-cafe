cec38446e2f65e8d6aa5d12e5c1d6e358eac596b
Oksana Zalivan <ksyushamartinovska@gmail.com>
2026-01-11 11:19:53 +0300

    complete cafe feedback app

---
+++ src/components/VoteStats/VoteStats.tsx
@@ -0,0 +1,32 @@
+import css from "./VoteStats.module.css";
+import type { Votes } from "../../types/votes";
+
+interface VoteStatsProps {
+    votes: Votes;
+    totalVotes: number;
+    positiveRate: number;
+}
+
+export default function VoteStats({ votes, totalVotes, positiveRate }: VoteStatsProps) {
+    const { good, neutral, bad } = votes;
+
+    return (
+        <div className={css.container}>
+            <p className={css.stat}>
+                Good: <strong>{good}</strong>
+            </p>
+            <p className={css.stat}>
+                Neutral: <strong>{neutral}</strong>
+            </p>
+            <p className={css.stat}>
+                Bad: <strong>{bad}</strong>
+            </p>
+            <p className={css.stat}>
+                Total: <strong>{totalVotes}</strong>
+            </p>
+            <p className={css.stat}>
+                Positive: <strong>{positiveRate}%</strong>
+            </p>
+        </div>
+    );
+}
